d2b803d5248ebb02e4bf977d1bcd88676fb15ee0
Adeel Ahmed <adeel@lessgo.co>
2022-10-09 22:30:25 +0500

    small fixes

--- genres-api/src/server.ts
+++ genres-api/src/server.ts
@@ -1,8 +1,11 @@
 import { logger } from './helpers/logger';
 import app from './index';
-const port = process.env.PORT
+import dotenv from "dotenv";
+
+dotenv.config();
+const port = process.env.PORT;
 
 logger.info(`Node environment: ${process.env.NODE_ENV}`);
 app.listen(port, () => {
-    logger.info(`Example app listening at port http://localhost:${port}`)
-}) 
+  logger.info(`Genre app listening at port http://localhost:${port}`);
+}); 
--- movies-api/src/server.ts
+++ movies-api/src/server.ts
@@ -1,5 +1,8 @@
 import { logger } from './helpers/logger';
 import app from './index';
+import dotenv from "dotenv";
+
+dotenv.config();
 const port = process.env.PORT
 
 logger.info(`Node environment: ${process.env.NODE_ENV}`);
